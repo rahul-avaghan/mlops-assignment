7699af05cd24336f8306656c6711117c8a3a0d3f
rahul avaghan <rahulbavaghan@gmail.com>
2024-07-29 15:18:39 +0200

    feat: set env

--- src/app.py
+++ src/app.py
@@ -9,12 +9,14 @@ app = Flask(__name__)
 model = LogisticRegression()
 model.fit(np.array([[0, 0], [1, 1]]), [0, 1])
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
     df = pd.DataFrame(data)
     prediction = model.predict(df)
     return jsonify({'prediction': prediction.tolist()})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
