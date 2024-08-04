cfd59e83fe29ddd0a93e244e13ef44fefb655bd0
Mohanen Balasubramanian <bmohanen@gmail.com>
2024-08-04 07:19:22 +0530

    mlflow tracking added

--- src/model.py
+++ src/model.py
@@ -1,6 +1,15 @@
 import joblib
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
+import mlflow
+import mlflow.sklearn
+# Set our tracking server uri for logging
+
+# Run this mlflow server in the terminal before starting the code
+# mlflow server --host 127.0.0.1 --port 8080
+# mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+mlflow.set_experiment("Iris classifier")
+mlflow.sklearn.autolog()
 
 # Load the iris dataset
 iris = load_iris()
